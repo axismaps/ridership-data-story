84b1f07bd598d5020217b74e2651b247b7c04135
David Heyman <dave@axismaps.com>
2019-04-11 10:51:10 +0100

    Load frame on change

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,13 +3,21 @@ function resize() {
 }
 
 $(document).ready(() => {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
+  const owl = $('.owl-carousel').owlCarousel({
     margin: 0,
     nav: true,
     navContainer: '.slide-nav',
     dotsContainer: '.slide-dots',
     items: 1,
+    callbacks: true,
   });
+
+  owl.on('changed.owl.carousel', function(e) {
+    const iframe = $($('.item')[e.item.index]).children('iframe');
+    iframe.attr('src', iframe.attr('data-src'));
+    const prevFrame = $($('.item')[e.item.index - 1]).children('iframe');
+    prevFrame.removeAttr('src');
+  });
+
   resize();
 });
